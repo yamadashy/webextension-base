b9b2a88cfc3eb39c0a2d817dc376b206389267b9
Kazuki Yamada <koukun0120@gmail.com>
2021-10-03 12:24:41 +0900

    update: index

--- index.ts
+++ index.ts
@@ -2,12 +2,8 @@ import { labeledLog } from './utils/console-logger';
 import { waitElementRender } from './utils/wait-element-render';
 import { isAlreadyRunningExtension } from './utils/extension-running-checker';
 
-const webExtensionBase = {
-  utils: {
-    labeledLog,
-    waitElementRender,
-    isAlreadyRunningExtension,
-  }
-};
-
-export default webExtensionBase;
+export const webExtensionUtils = {
+  labeledLog,
+  waitElementRender,
+  isAlreadyRunningExtension,
+}
